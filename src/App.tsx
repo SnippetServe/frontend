d86b994825849771a9a5bbc8136f3db8f756eca6
rohitkrishna094 <rohitkrishna094@gmail.com>
2020-08-28 01:51:56 -0400

    added test message caption

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>SnippetServe</h1>
+      <p>A platform to share your code snippets.</p>
     </div>
   );
 }
