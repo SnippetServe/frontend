0afc3a3b0a8e80d12c16b56b430ffd66c1863f55
furkanmavili <furkanmavili@outlook.com>
2020-08-29 19:49:09 +0300

    Export assets

---
+++ src/assets/images/index.tsx
@@ -0,0 +1,25 @@
+import AndroidBig from "./android_big.png";
+import AndroidSmall from "./android_small.png";
+import AtomBig from "./atom_big.png";
+import AtomSmall from "./atom_small.png";
+import IntelIJBig from "./intelij_big.png";
+import IntelIJSmall from "./intelij_small.png";
+import PycharmBig from "./pycharm_big.png";
+import PycharmSmall from "./pycharm_small.png";
+import {
+  default as VsCodeBig,
+  default as VsCodeSmall,
+} from "./vscode_small.png";
+
+export {
+  AndroidSmall,
+  AndroidBig,
+  AtomBig,
+  AtomSmall,
+  IntelIJBig,
+  IntelIJSmall,
+  PycharmBig,
+  PycharmSmall,
+  VsCodeBig,
+  VsCodeSmall,
+};
