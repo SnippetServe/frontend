2f296b66a176688e68587f500359c370da9986ba
furkanmavili <furkanmavili@outlook.com>
2020-09-03 17:45:51 +0300

    Fix header for mobile screens

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -1,34 +1,46 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import {
   Box,
-  IconButton,
+  Icon,
   Menu,
   MenuButton,
   MenuItem,
   MenuList
 } from '@chakra-ui/core';
 import React from 'react';
+import CustomLink from './CustomLink';
 
 function DropdownMenu({ ...props }: any) {
   return (
     /* according to eslint, props spreading below is a bad practice. For now, I've disabled the
      * rule for only this file as can be seen by the rule mentioned at the top of this current file
      */
-    <Box color="black" {...props}>
+    <Box color="black" w="50px" {...props}>
       <Menu>
-        <MenuButton>
-          <IconButton
-            icon="triangle-down"
-            aria-label="triangle-down"
-            variant="ghost"
-            color="white"
-          />
+        <MenuButton
+          px={4}
+          py={2}
+          transition="all 0.2s"
+          rounded="md"
+          _expanded={{ bg: 'blue.400' }}
+          _focus={{ outline: 'none', boxShadow: 'outline' }}
+        >
+          <Icon name="chevron-down" color="white" size="20px" />
         </MenuButton>
-        <MenuList>
-          <MenuItem>Snippets</MenuItem>
-          <MenuItem>Extensions</MenuItem>
-          <MenuItem>SignIn</MenuItem>
-          <MenuItem>SignUp</MenuItem>
+
+        <MenuList left={0}>
+          <MenuItem>
+            <CustomLink to="/snippets">Snippets</CustomLink>
+          </MenuItem>
+          <MenuItem>
+            <CustomLink to="/extensions">Extensions</CustomLink>
+          </MenuItem>
+          <MenuItem>
+            <CustomLink to="/signin">Sign up</CustomLink>
+          </MenuItem>
+          <MenuItem>
+            <CustomLink to="/signup">Sign up</CustomLink>
+          </MenuItem>
         </MenuList>
       </Menu>
     </Box>
