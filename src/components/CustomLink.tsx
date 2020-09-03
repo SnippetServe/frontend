d92053bacb898b22f1909f182cd127ae1c624e6f
furkanmavili <furkanmavili@outlook.com>
2020-09-03 17:49:26 +0300

    Add style to customlink component

--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -7,9 +7,14 @@ import { Link as ChakraLink } from '@chakra-ui/core';
 import * as React from 'react';
 import { Link as RouterLink, LinkProps } from 'react-router-dom';
 
+const style = {
+  textDecoration: 'none',
+  width: '100%',
+  display: 'inline-block'
+};
 const Link = (props: LinkProps) => (
   // @ts-ignore
-  <ChakraLink {...props} as={RouterLink} style={{ textDecoration: 'none' }} />
+  <ChakraLink {...props} as={RouterLink} style={style} />
 );
 
 export default Link;
