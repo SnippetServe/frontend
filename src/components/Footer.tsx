60dde14b8a6057439f904b5f04d6c1848d595243
furkanmavili <furkanmavili@outlook.com>
2020-09-03 17:43:54 +0300

    Responsive Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,13 +4,15 @@ import React from 'react';
 function Footer() {
   return (
     <Flex
-      h="100px"
+      h={['60px', '100px']}
       bg="#1D1A1A"
       color="white"
       justifyContent="center"
       alignItems="center"
     >
-      <Text>© 2020, DevHyperCoder. All Rights Reserved</Text>
+      <Text fontSize={['sm', 'md']}>
+        © 2020, DevHyperCoder. All Rights Reserved
+      </Text>
     </Flex>
   );
 }
